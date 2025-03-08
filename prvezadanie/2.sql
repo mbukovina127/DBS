29b7162772d08bcf0022350a566eb56ddda04415
TINKPAD <xbukovinam@stuba.sk>
2025-03-08 16:47:40 +0100

    2 uloha done

--- prvezadanie/2.sql
+++ prvezadanie/2.sql
@@ -1,33 +1,53 @@
-
-select id, full_name, team_changes from players
-JOIN (
-    SELECT 
-        player_id, 
-        COUNT(distinct team_id) as team_changes
+SELECT 
+    p.id AS player_id,
+    p.first_name,
+    p.last_name,
+    t.id,
+    t.full_name AS team_name,
+	ROUND(
+	    CAST(SUM(CASE  
+	        WHEN pr.event_msg_type = 'FIELD_GOAL_MADE' AND pr.player1_id = p.id THEN 2
+	        WHEN pr.event_msg_type = 'FREE_THROW' AND pr.player1_id = p.id AND pr.score IS NOT NULL THEN 1 
+	        ELSE NULL 
+	    END) AS numeric) 
+	    / NULLIF(COUNT(DISTINCT pr.game_id), 0), 
+	    2
+	) AS PPG,
+	
+	ROUND(
+	    CAST(SUM(CASE 
+	        WHEN pr.event_msg_type = 'FIELD_GOAL_MADE' AND pr.player2_id = p.id THEN 1 
+	        ELSE NULL 
+	    END) AS numeric) 
+	    / NULLIF(COUNT(DISTINCT pr.game_id), 0), 
+	    2
+	) AS APG,
+    COUNT(DISTINCT pr.game_id) AS games
+FROM play_records pr
+JOIN games g ON pr.game_id = g.id
+JOIN players p ON pr.player1_id = p.id OR pr.player2_id = p.id
+JOIN teams t ON pr.player1_team_id = t.id or pr.player2_team_id = t.id
+WHERE g.season_id = '22017'
+AND pr.event_msg_type IN ('FREE_THROW', 'FIELD_GOAL_MADE', 'FIELD_GOAL_MISSED', 'REBOUND')
+AND p.id IN (
+    SELECT player_id
     FROM (
-        SELECT 
-            player1_id AS player_id, 
-            player1_team_id AS team_id
-        FROM play_records r
-        JOIN games g ON r.game_id = g.id
-        WHERE g.season_id = '22017' 
-            AND r.player1_id IS NOT NULL
-            AND r.event_msg_type IN ('FREE_THROW', 'FIELD_GOAL_MADE', 'FIELD_GOAL_MISSED', 'REBOUND')
-        UNION
-        SELECT 
-            player2_id AS player_id, 
-            player2_team_id AS team_id
-        FROM play_records r
-        JOIN games g ON r.game_id = g.id
-        WHERE g.season_id = '22017' 
-            AND r.player2_id IS NOT NULL
-            AND r.event_msg_type IN ('FREE_THROW', 'FIELD_GOAL_MADE', 'FIELD_GOAL_MISSED', 'REBOUND')
+        SELECT player1_id AS player_id, player1_team_id AS team_id FROM play_records pr1
+        JOIN games g1 ON pr1.game_id = g1.id
+        WHERE g1.season_id = '22017'
+        AND pr1.event_msg_type IN ('FREE_THROW', 'FIELD_GOAL_MADE', 'FIELD_GOAL_MISSED', 'REBOUND')
+        
+        UNION ALL
+        
+        SELECT player2_id AS player_id, player2_team_id AS team_id FROM play_records pr2
+        JOIN games g2 ON pr2.game_id = g2.id
+        WHERE g2.season_id = '22017'
+        AND pr2.event_msg_type IN ('FREE_THROW', 'FIELD_GOAL_MADE', 'FIELD_GOAL_MISSED', 'REBOUND')
     ) AS player_teams
-	group by player_id
-	order by team_changes desc
-	limit 5
-) pr ON pr.player_id = id
-order by id
-
-
-
+    GROUP BY player_id
+    HAVING COUNT(DISTINCT team_id) > 1
+    ORDER BY COUNT(DISTINCT team_id) DESC
+    LIMIT 5
+)
+GROUP BY p.id, p.first_name, p.last_name, t.id, t.full_name
+ORDER BY p.id ASC, t.id ASC;
--- prvezadanie/3.sql
+++ prvezadanie/3.sql
@@ -1,45 +1,13 @@
-SELECT 
-    p.id AS player_id,
-    p.first_name,
-    p.last_name,
-    t.id,
-    t.full_name AS team_name,
-    
-	SUM(CASE WHEN pr.event_msg_type = 'FIELD_GOAL_MADE' THEN 2 ELSE NULL END) 
-          / COUNT(DISTINCT pr.game_id) 
-		  AS PPG,
-    SUM(CASE 
-			WHEN pr.event_msg_type = 'FIELD_GOAL_MADE' and pr.player2_id = p.id
-			THEN 1 
-			ELSE NULL END) 
-	  / COUNT(DISTINCT pr.game_id)
-	  AS APG,
-    COUNT(DISTINCT pr.game_id) AS games
-FROM play_records pr
-JOIN games g ON pr.game_id = g.id
-JOIN players p ON pr.player1_id = p.id OR pr.player2_id = p.id
-JOIN teams t ON pr.player1_team_id = t.id or pr.player2_team_id = t.id
-WHERE g.season_id = '22017'
-AND pr.event_msg_type IN ('FREE_THROW', 'FIELD_GOAL_MADE', 'FIELD_GOAL_MISSED', 'REBOUND')
-AND p.id IN (
-    SELECT player_id
-    FROM (
-        SELECT player1_id AS player_id, player1_team_id AS team_id FROM play_records pr1
-        JOIN games g1 ON pr1.game_id = g1.id
-        WHERE g1.season_id = '22017'
-        AND pr1.event_msg_type IN ('FREE_THROW', 'FIELD_GOAL_MADE', 'FIELD_GOAL_MISSED', 'REBOUND')
-        
-        UNION ALL
-        
-        SELECT player2_id AS player_id, player2_team_id AS team_id FROM play_records pr2
-        JOIN games g2 ON pr2.game_id = g2.id
-        WHERE g2.season_id = '22017'
-        AND pr2.event_msg_type IN ('FREE_THROW', 'FIELD_GOAL_MADE', 'FIELD_GOAL_MISSED', 'REBOUND')
-    ) AS player_teams
-    GROUP BY player_id
-    HAVING COUNT(DISTINCT team_id) > 1
-    ORDER BY COUNT(DISTINCT team_id) DESC
-    LIMIT 5
-)
-GROUP BY p.id, p.first_name, p.last_name, t.id, t.full_name
-ORDER BY p.id ASC, t.id ASC;
+SELECT
+	*
+	-- p.id,
+	-- event_number,
+	-- event_msg_type,
+	-- SPLIT_PART(score, '-', 1) as left_score,
+	-- SPLIT_PART(score, '-', 2) as right_score,
+FROM play_records r
+join players p
+on p.id = r.player1_id or p.id = r.player2_id or p.id = r.player3_id
+where r.game_id = 21701185 and score is not null
+order by event_number asc
+	
