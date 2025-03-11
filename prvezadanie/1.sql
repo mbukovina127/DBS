685af95e25d80c3559eee565e4ec59cb3f69c30e
TINKPAD <xbukovinam@stuba.sk>
2025-03-11 11:53:04 +0100

    1 fixed 2 wrong

--- prvezadanie/1.sql
+++ prvezadanie/1.sql
@@ -1,11 +1,17 @@
-select p.id, p.first_name, p.last_name, r1.period, r1.wctimestring as period_time from play_records r1
+select 
+	p.id, 
+	p.first_name, 
+	p.last_name, 
+	r1.period, 
+	r1.pctimestring as period_time 
+from play_records r1
 join players p on r1.player1_id = p.id
 join play_records r2
 on r1.game_id = r2.game_id
 and r1.player1_id = r2.player1_id
-and r1.event_msg_type = 'REBOUND'
-and r2.event_msg_type = 'FIELD_GOAL_MADE'
-AND r1.event_number = r2.event_number -1
+and r1.event_msg_type = 'FIELD_GOAL_MADE'
+and r2.event_msg_type = 'REBOUND'
+AND r1.event_number -1= r2.event_number
 where r1.game_id = 22000529
 -- where r1.game_id = {{game_id}}
 order by r1.period ASC, r1.pctimestring DESC, p.id ASC
--- prvezadanie/3.sql
+++ prvezadanie/3.sql
@@ -1,10 +1,19 @@
 SELECT
-	*
-	-- p.id,
-	-- event_number,
-	-- event_msg_type,
-	-- SPLIT_PART(score, '-', 1) as left_score,
-	-- SPLIT_PART(score, '-', 2) as right_score,
+	-- *
+	p.id,
+	event_number,
+	event_msg_type,
+	score,
+	score_margin,
+		(CASE 
+		    WHEN score_margin = 'TIE' THEN 0  -- Convert 'TIE' to 0
+		    ELSE NULLIF(score_margin, '')::INTEGER  -- Convert numbers, ignore empty strings
+		END) - 
+	LAG(CASE 
+	        WHEN score_margin = 'TIE' THEN 0  
+	        ELSE NULLIF(score_margin, '')::INTEGER  
+	    END) OVER (PARTITION BY r.game_id ORDER BY r.event_number) 
+	AS diff
 FROM play_records r
 join players p
 on p.id = r.player1_id or p.id = r.player2_id or p.id = r.player3_id
